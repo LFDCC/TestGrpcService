f69b6f4d5fbb8e79f2fa9bb5a4fd4fd5c123fb2f
yanqi <1584329729@qq.com>
2022-10-28 16:11:53 +0800

    111

--- TestGrpcNetClient/Program.cs
+++ TestGrpcNetClient/Program.cs
@@ -1,14 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using Grpc.Core;
 using Grpc.Net.Client;
+
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 
 using TestGrpcNetClient;
 
-using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-var client = new Greeter.GreeterClient(channel);
-var res = await client.SayHelloAsync(new HelloRequest
+var options = new JsonSerializerOptions
 {
-    Name = "哈哈哈1"
-});
-Console.WriteLine(res.Message);
+    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+};
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7128");
+var client = new Greeter.GreeterClient(channel);
+
+#region 一元方法
+{
+    Console.WriteLine($"=====================================一元方法-BEGIN==============================================");
+    var res = await client.SayHelloAsync(new HelloRequest
+    {
+        Name = "哈哈哈1"
+    });
+    Console.WriteLine($"这是一元方法：{JsonSerializer.Serialize(res, options)}");
+    Console.WriteLine($"=====================================一元方法-END==============================================");
+    Console.WriteLine();
+}
+#endregion
+
+#region 服务器向客户端推流
+{
+    Console.WriteLine($"=====================================服务器向客户端推流-BEGIN==============================================");
+    using var call = client.StreamingFromServer(new ExampleRequest { PageIndex = 1, PageSize = 3, IsDescending = true });
+
+    while (await call.ResponseStream.MoveNext())
+    {
+        Console.WriteLine("接收的服务器流: " + JsonSerializer.Serialize(call.ResponseStream.Current, options));
+    }
+    Console.WriteLine($"=====================================服务器向客户端推流-END==============================================");
+    Console.WriteLine();
+}
+#endregion
+
+#region 客户端向服务器推流
+{
+    Console.WriteLine($"=====================================客户端向服务器推流-BEGIN==============================================");
+    using var clientStream = client.StreamingFromClient();
+
+    for (var i = 0; i < 5; i++)
+    {
+        await clientStream.RequestStream.WriteAsync(new ExampleRequest { PageIndex = i, PageSize = i, IsDescending = true });
+
+    }
+    await clientStream.RequestStream.CompleteAsync();
+
+    var response = await clientStream;
+    Console.WriteLine($"服务器返回: {response.Name}");
+    Console.WriteLine($"=====================================客户端向服务器推流-END==============================================");
+    Console.WriteLine();
+}
+#endregion
+
+#region 双向流
+{
+    Console.WriteLine($"=====================================双向流-BEGIN==============================================");
+    using var bothStream = client.StreamingBothWays();
+
+    //向服务器推流
+    for (var i = 0; i < 3; i++)
+    {
+        await bothStream.RequestStream.WriteAsync(new ExampleRequest { PageIndex = i + 100, PageSize = i + 100, IsDescending = true });
+    }
+    await bothStream.RequestStream.CompleteAsync();
+    //接收服务器流
+    while (await bothStream.ResponseStream.MoveNext())
+    {
+        Console.WriteLine("接收的服务器流: " + JsonSerializer.Serialize(bothStream.ResponseStream.Current, options));
+    }
+    Console.WriteLine($"=====================================双向流-END==============================================");
+}
+#endregion
+
 Console.ReadKey();
--- TestGrpcNetCoreService/Services/GreeterService.cs
+++ TestGrpcNetCoreService/Services/GreeterService.cs
@@ -1,15 +1,31 @@
 using Grpc.Core;
 
+using Microsoft.Extensions.Options;
+
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
 namespace TestGrpcNetCoreService.Services
 {
     public class GreeterService : Greeter.GreeterBase
     {
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+        };
+
         private readonly ILogger<GreeterService> _logger;
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
         }
-
+        /// <summary>
+        /// 一元方法
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
@@ -18,6 +34,12 @@ namespace TestGrpcNetCoreService.Services
             });
         }
 
+        /// <summary>
+        /// 返回空对象
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public override Task<Empty> EmptyHello(EmptyHelloRequest request, ServerCallContext context)
         {
             foreach (var item in request.Peoples)
@@ -25,6 +47,68 @@ namespace TestGrpcNetCoreService.Services
 
             }
             return Task.FromResult(new Empty());
+        }
+
+        /// <summary>
+        /// 服务器流式处理方法
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="responseStream"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task StreamingFromServer(ExampleRequest request, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("服务器流开始响应。。。。");
+            for (int i = 0; i < 5; i++)
+            {
+                await responseStream.WriteAsync(new ExampleResponse()
+                {
+                    Name = $"qq{i}",
+                    Sex = $"woman{i}"
+                });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+            Console.WriteLine("服务器流结束响应。。。。");
+        }
+
+        /// <summary>
+        /// 客户端流式处理方法
+        /// </summary>
+        /// <param name="requestStream"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<ExampleResponse> StreamingFromClient(IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
+        {
+            Console.WriteLine("服务器开始响应客户端流。。。。");
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine("接收的客户端流: " + JsonSerializer.Serialize(requestStream.Current, options));
+            }
+            return new ExampleResponse
+            {
+                Name = "客户端流结束"
+            };
+        }
+
+        /// <summary>
+        /// 双向流式处理方法
+        /// </summary>
+        /// <param name="requestStream"></param>
+        /// <param name="responseStream"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("双向流开始响应。。。。");
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine("接收的客户端流: " + JsonSerializer.Serialize(requestStream.Current, options));
+                await responseStream.WriteAsync(new ExampleResponse()
+                {
+                    Name = JsonSerializer.Serialize(requestStream.Current, options)
+                });
+            }
+            Console.WriteLine("双向流结束响应。。。。");
         }
     }
 }
